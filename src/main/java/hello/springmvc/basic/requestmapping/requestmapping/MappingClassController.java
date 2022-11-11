db0d9fdcfb7e0e7ae52bd91578db61412c3b0e0d
Lego79 <kentcandoit89@gmail.com>
2022-11-11 15:08:15 +0900

    rest API controller

---
+++ src/main/java/hello/springmvc/basic/requestmapping/requestmapping/MappingClassController.java
@@ -0,0 +1,37 @@
+package hello.springmvc.basic.requestmapping.requestmapping;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingClassController {
+
+
+    @GetMapping("/mapping/users")
+    public String user() {
+        return "get users";
+    }
+
+    @PostMapping("/mapping/users")
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/mapping/users/{userId}")
+    public String findUser(@PathVariable String userId) {
+
+        return "get userId=" + userId;
+
+    }
+
+    @PatchMapping("/mapping/users/{userId}")
+        public String updateUser(@PathVariable String userId) {
+        return "update UserId" + userId;
+        }
+
+    @DeleteMapping("/mapping/users/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete UserId" + userId;
+    }
+
+}
