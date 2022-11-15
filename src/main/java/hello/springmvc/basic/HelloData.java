876c25d66d4be3172cc4f3e0e7c6d99222b09bb5
Lego79 <kentcandoit89@gmail.com>
2022-11-15 11:07:37 +0900

    http request json controller

---
+++ src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+
+@Data // 게터 세터, 투스트링, 이콜 해쉬코드 ,requiredargsconstructor
+public class HelloData {
+
+    private String username;
+    private int age;
+
+
+
+}
